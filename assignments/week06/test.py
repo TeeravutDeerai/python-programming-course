d84cd2558051808ae2eb49c244d1060627650083
Teeravut Deerai <teeravut.d@ku.th>
2026-08-07 10:04:53 +0700

    อยู่ไม่ไหว อยู่ไม่ไหว

---
+++ assignments/week06/test.py
@@ -0,0 +1,16 @@
+# เขียน function ชื่อ calculate_sphere(radius):
+# คำนวณหา ปริมาตร ของทรงกลม
+# volume = 4.0 / 3 * pi * radius ** 3
+# จากนั้นแสดงผลลัพธ์ที่เหมาะสมออกทางหน้าจอ
+
+import math
+ 
+def calculate_sphere(radius):
+    volume = (4.0 / 3) * math.pi * (radius ** 3)
+    return volume
+ 
+# โปรแกรมทดสอบ
+radius = float(input("Enter radius: "))
+volume = calculate_sphere(radius)
+ 
+print("Volume of sphere =", volume)
